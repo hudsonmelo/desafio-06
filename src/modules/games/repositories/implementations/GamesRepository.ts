cae6caa032edeb30367e9cfcf00825bf09bbb7a1
Hudson Melo <hudsonmn@outlook.com>
2021-06-29 06:30:47 -0300

    desafio-06

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,25 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const games = await this.repository
+      .createQueryBuilder("game")
+      .where("game.title ILIKE :title", { title: `%${param}%`})
+      .getMany();
+    
+    return games
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT COUNT(id) FROM games`);
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
+    const users = await this.repository
       .createQueryBuilder()
-      // Complete usando query builder
+      .relation(Game, "users")
+      .of(id)
+      .loadMany();
+    
+      return users
   }
 }
